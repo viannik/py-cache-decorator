dafeae339e1d0c0bbd228dc924a5a5e0ca559b9c
viannik <ivan.nikitski@gmail.com>
2025-06-21 14:05:27 +0300

    Implement caching decorator to optimize function calls

--- app/main.py
+++ app/main.py
@@ -1,6 +1,20 @@
+from functools import wraps
 from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_dict = {}
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        key = args + tuple(sorted(kwargs.items()))
+        if key in cache_dict:
+            print("Getting from cache")
+            return cache_dict[key]
+        else:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            cache_dict[key] = result
+            return result
+
+    return wrapper
